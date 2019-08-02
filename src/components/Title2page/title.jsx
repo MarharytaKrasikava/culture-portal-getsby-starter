392a593f99771e49c28b547a6a417aa471cc5a5b
Pavel Yermakov <pavelak137@mail.ru>
2019-08-02 12:41:03 +0300

    fix: title2page

--- src/components/Title2page/title.jsx
+++ src/components/Title2page/title.jsx
@@ -1,34 +1,35 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 
 import React from 'react';
 import PropTypes from 'prop-types';
 import './title.css';
-import authors from '../authors';
 import authorImage from '../../images/Authors/KorshSablin/KorshSablin.jpg';
 
-export default function TitleTwoPage() {
+export default function TitleTwoPage(props) {
   return (
     <React.Fragment>
       <div className="title">
 
         <div className="image-title"><img src={authorImage} alt="1" /></div>
         <div className="name-title">
-          {authors.KorshSablin.name}
+          {props.author.name}
           {' '}
 
           {' '}
-          {authors.KorshSablin.surname}
+          {props.author.surname}
         </div>
         <div className="age-title">
 (
-          {authors.KorshSablin.birthDate}
+          {props.author.birthDate}
           {' '}
 -
           {' '}
-          {authors.KorshSablin.deathDate}
+          {props.author.deathDate}
 )
         </div>
-        <div className="information-title">{authors.KorshSablin.litleText}</div>
+        <div className="information-title">{props.author.titleText}</div>
       </div>
 
     </React.Fragment>
