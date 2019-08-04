aa2eaecee60534a4532a3ce661d19fad649d79a8
hhh1361 <nikita.turlyko@gmail.com>
2019-08-04 20:47:10 +0300

    feat: second page styles and adaptive

--- src/components/Timeline/timeline.jsx
+++ src/components/Timeline/timeline.jsx
@@ -8,16 +8,17 @@ import './timeline.css';
 
 export default function TimelineComponent({ timeline }) {
   const colorArray = [
-    '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
-    '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
-    '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
-    '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
-    '#66994D', '#B366CC', '#4D8000', '#B33300', '#CC80CC',
-    '#66664D', '#991AFF', '#E666FF', '#4DB3FF', '#1AB399',
-    '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
-    '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
-    '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-    '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+    '#999999',
+    '#888888',
+    '#777777',
+    '#666666',
+    '#555555',
+    '#444444',
+    '#333333',
+    '#222222',
+    '#111111',
+    '#111111',
+  ];
   return (
     <React.Fragment>
       <Timeline lineColor="#ddd">
@@ -25,7 +26,10 @@ export default function TimelineComponent({ timeline }) {
           <TimelineItem
             key={element.description}
             dateText={element.date}
-            dateInnerStyle={{ background: colorArray[index], color: '#333' }}
+            dateInnerStyle={{
+              background: 'radial-gradient(#e6dada, #274046)', color: '#532626', textAlign: 'center', fontSize: '1.2rem',
+            }}
+            style={{ color: colorArray[index] }}
           >
             {element.description}
           </TimelineItem>
