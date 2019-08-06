c017ca89648c7d9e0b581b79d5cc45e4c266e1f7
Marharyta Krasikava <marharytakrasikava@gmail.com>
2019-08-06 13:11:26 +0300

    fix: return config to the previous state

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,27 @@ module.exports = {
         path: `${__dirname}/src/pages/`,
       },
     },
+    'gatsby-transformer-sharp',
+    {
+      resolve: 'gatsby-plugin-sharp',
+      options: {
+        useMozJpeg: false,
+        stripMetadata: true,
+        defaultQuality: 75,
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#663399',
+        theme_color: '#663399',
+        display: 'minimal-ui',
+        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+      },
+    },
     'gatsby-plugin-netlify-cms',
     'gatsby-transformer-remark',
   ],
